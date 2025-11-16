1f9a44b7c05be2cd2f78562035608988e593f4fe
Vlad <vladh3662@gmail.com>
2025-11-16 19:44:15 +0200

    Add authentication

---
+++ nextjs-start/src/lib/firebase/auth.js
@@ -0,0 +1,34 @@
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged as _onAuthStateChanged,
+  onIdTokenChanged as _onIdTokenChanged,
+} from "firebase/auth";
+
+import { auth } from "@/src/lib/firebase/clientApp";
+
+export function onAuthStateChanged(cb) {
+  return _onAuthStateChanged(auth, cb);
+}
+
+export function onIdTokenChanged(cb) {
+  return _onIdTokenChanged(auth, cb);
+}
+
+export async function signInWithGoogle() {
+  const provider = new GoogleAuthProvider();
+
+  try {
+    await signInWithPopup(auth, provider);
+  } catch (error) {
+    console.error("Error signing in with Google", error);
+  }
+}
+
+export async function signOut() {
+  try {
+    return auth.signOut();
+  } catch (error) {
+    console.error("Error signing out with Google", error);
+  }
+}
